efea5c494e808c0b2fa38147f9397bcb420e63cc
alexandrelucas <38571147+alexandrelucas@users.noreply.github.com>
2020-02-13 22:49:17 -0300

    Teste de Paginação

---
+++ PaginationFlowLayout/PaginationFlowLayout/FrmMain.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace PaginationFlowLayout
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutDisplay = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // flowLayoutDisplay
+            // 
+            this.flowLayoutDisplay.Location = new System.Drawing.Point(12, 12);
+            this.flowLayoutDisplay.Name = "flowLayoutDisplay";
+            this.flowLayoutDisplay.Size = new System.Drawing.Size(559, 220);
+            this.flowLayoutDisplay.TabIndex = 0;
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(496, 238);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 23);
+            this.btnNext.TabIndex = 1;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(415, 238);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(75, 23);
+            this.btnBack.TabIndex = 2;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.Location = new System.Drawing.Point(335, 241);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(74, 16);
+            this.lblStatus.TabIndex = 0;
+            this.lblStatus.Text = "Page: 0/0";
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 273);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.flowLayoutDisplay);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Paging Flow Layout Example";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutDisplay;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
---
+++ PaginationFlowLayout/PaginationFlowLayout/FrmMain.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PaginationFlowLayout
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private List<Button> buttons = new List<Button>();
+        private int currentPage = 1;
+        private int btnPerPage = 16;
+        private int countPages = 0;
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            for(int i = 0; i < 100; i++)
+            {
+                var btn = new Button();
+                btn.Text = $"Button {i}";
+                btn.Name = $"btn_{i}";
+                btn.Size = new Size(60, 40);
+                btn.Click += Btn_Click;
+                buttons.Add(btn);
+                if (i < btnPerPage)
+                    flowLayoutDisplay.Controls.Add(btn);
+            }
+            UpdateStatus();
+
+        }
+        private void UpdateStatus()
+        {
+            countPages = (int)Math.Ceiling(Convert.ToDecimal(buttons.Count) / Convert.ToDecimal(btnPerPage));
+            //    1/7
+            lblStatus.Text = $"Page {currentPage}/{countPages}";
+
+            if (currentPage == 1)
+                btnBack.Enabled = false;
+            else if (currentPage == countPages)
+            {
+                btnBack.Enabled = true;
+                btnNext.Enabled = false;
+            }
+            else
+            {
+                btnBack.Enabled = true;
+                btnNext.Enabled = true;
+            }
+
+            if (countPages <= 1)
+                btnNext.Enabled = false;
+
+        }
+
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            Button btnItem = (Button)sender;
+            btnItem.Text = btnItem.Name;
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            flowLayoutDisplay.Controls.Clear();
+            currentPage--;
+            for (int i = (currentPage - 1) * btnPerPage; i < (currentPage * btnPerPage); i++)
+            {
+                if (i == btnPerPage - 1)
+                    btnBack.Enabled = false;
+                flowLayoutDisplay.Controls.Add(buttons[i]);
+            }
+            UpdateStatus();
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            flowLayoutDisplay.Controls.Clear();
+            currentPage++;
+            for (int i = (currentPage - 1) * btnPerPage; i < (currentPage * btnPerPage); i++)
+            {
+
+                if (i < buttons.Count)
+                    flowLayoutDisplay.Controls.Add(buttons[i]);
+            }
+            UpdateStatus();
+        }
+    }
+}
